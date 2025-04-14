6c781679445b8c49ea6c1a894179ea11e1534630
Wesley Bezerra <mouraneta7@gmail.com>
2025-04-14 20:38:11 -0300

    Criação da estutura do projeto + repositorio, serviço e modelo

---
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -0,0 +1,8 @@
+package br.com.anm.projeto_crud.produtos.controle;
+ 
+ import org.springframework.web.bind.annotation.RestController;
+ 
+ @RestController
+ public class ProdutoControle {
+  
+ }
---
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/servico/ProdutoServico.java
@@ -0,0 +1,8 @@
+package br.com.anm.projeto_crud.produtos.servico;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoServico {
+
+}
